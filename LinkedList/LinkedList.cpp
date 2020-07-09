b1430aef66c2f676543b1efe4f73963493546b86
Harshit Sharma <harshit@QMAC0321.local>
2020-07-10 00:37:23 +0530

    Linked List

---
+++ LinkedList/LinkedList.cpp
@@ -0,0 +1,22 @@
+// Author : Harshit Sharma
+// Linked list Implementation
+
+#include<iostream>
+#define ll long long int
+using namespace std;
+
+class Node {
+    public :
+        ll data;
+        Node * next;
+        Node(ll data){
+            data = data;
+        }
+};
+
+class LinkedList {
+    public :
+        Node * head;
+        
+};
+
