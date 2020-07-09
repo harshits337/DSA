9326524d02f3953f611823d86d96a12d1fc54504
Harshit Sharma <harshit@QMAC0321.local>
2020-07-10 04:29:30 +0530

    Linked List Insertion Operations

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -1,5 +1,5 @@
 // Author : Harshit Sharma
-// Linked list Implementation
+// Linked List Implementation for insertion operations
 
 #include<iostream>
 #define ll long long int
@@ -9,14 +9,85 @@ class Node {
     public :
         ll data;
         Node * next;
-        Node(ll data){
-            data = data;
+        Node(ll value){
+            data = value;
+            next = NULL;
         }
 };
 
-class LinkedList {
+class LinkedList{
     public :
-        Node * head;
-        
+        Node * head = NULL;
+        void insert(ll data);
+        void insertAtFront(ll data);
+        void insertAtPosition(ll data, ll pos);
+        void display();
 };
 
+void LinkedList :: insert(ll data){
+    Node * newNode = new Node(data);
+
+    if(head == NULL){
+        head = newNode;
+        return;
+    }
+
+    Node * temp = head;
+    while(temp->next){
+        temp = temp ->next;
+    }
+    temp->next = newNode;
+
+}
+
+void LinkedList :: display(){
+    Node * temp = head;
+    while(temp){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+void LinkedList :: insertAtFront(ll data){
+    Node * newNode = new Node(data);
+    if(head == NULL){
+        head = newNode;
+        return;
+    }
+    newNode ->next = head;
+    head = newNode;
+}
+
+void LinkedList :: insertAtPosition(ll data, ll pos){
+
+    if(pos==0){
+        insertAtFront(data);
+        return;
+    }
+    ll count = 0;
+    Node * temp = head;
+    while(temp->next && count<pos-1){
+        count++;
+        temp = temp->next;
+    }
+    Node * newNode = new Node(data);
+    newNode ->next = temp->next;
+    temp->next = newNode;
+}
+
+int main(){
+
+    LinkedList linkedList;
+    linkedList.insert(1);
+    linkedList.insert(2);
+    linkedList.insert(3);
+    linkedList.display();
+    linkedList.insertAtFront(4);
+    linkedList.display();
+    // 0 based indexing
+    linkedList.insertAtPosition(5,0);
+    linkedList.display();
+
+
+}
